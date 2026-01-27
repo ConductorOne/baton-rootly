807d112dcc78087cb28f17be272f30008f101f21
Steve Gontzes <steve.gontzes@conductorone.com>
2026-01-27 18:47:54 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2 (#57)

--- cmd/baton-rootly/main.go
+++ cmd/baton-rootly/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/conductorone/baton-rootly/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -24,6 +25,7 @@ func main() {
 		"baton-rootly",
 		getConnector,
 		cfg.Config,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
